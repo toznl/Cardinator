0952e2288c9a2cd06a44a6f0c1a7179a44076b02
Seo BoHyeong <dontsay_no@nate.com>
2018-04-02 15:16:50 +0900

    -Latest Version

--- Cardinator/MainWindow.xaml.cs
+++ Cardinator/MainWindow.xaml.cs
@@ -35,6 +35,57 @@ namespace Cardinator
             RunServer sv = new RunServer();
             root.Children.Add(sv);
             this.Closing += delegate { Environment.Exit(1); };
+
+            TcpListener server = null;
+
+            try
+            {
+                server = new TcpListener(IPAddress.Parse("127.0.0.1"), 8080);
+                server.Start();
+
+                byte[] buffer = new byte[1024];
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection.....");
+
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("\nConnected!!");
+
+                    NetworkStream stream = client.GetStream();
+
+                    while (stream.Read(buffer, 0, buffer.Length) != 0)
+                    {
+                        // deserializing;
+                        CoOrd packet = new CoOrd();
+                        packet.Deserialize(ref buffer);
+
+                        float x = packet.x;
+                        float y = packet.y;
+                        float z = packet.z;
+
+                        Console.WriteLine("===========================================");
+                        Console.WriteLine("X : {0}", x);
+                        Console.WriteLine("Y : {0}", y);
+                        Console.WriteLine("Z : {0}", z);
+                        Console.WriteLine("===========================================");
+                    }
+
+                    stream.Close();
+                    client.Close();
+                }
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine(se.Message.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+            Console.ReadLine();
+
         }
 
        
